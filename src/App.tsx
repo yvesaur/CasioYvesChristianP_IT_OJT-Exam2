c8e2697ba7b5f5a4cee779609e9f214f80762983
Yves Casio <yveschristianpcasio@gmail.com>
2023-09-14 14:51:06 +0800

    style: :lipstick: Navigation bar UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className=" text-red ">Omsim Barabida</h1>
+      <Navbar></Navbar>
     </>
   );
 }
