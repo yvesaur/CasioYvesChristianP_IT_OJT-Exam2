4134de1af2062eabac213d3a076589dfa0fbee6f
Yves Casio <yveschristianpcasio@gmail.com>
2023-09-15 05:25:23 +0800

    fix: :bug: Show news data now displays the data of specific clicked news article

--- src/Emails.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Emails = () => {
-  return <div>emails</div>;
-};
-
-export default Emails;
--- src/components/News.tsx
+++ src/components/News.tsx
@@ -18,13 +18,13 @@ interface newsDummyData {
 
 const News: React.FC<newsDummyData> = ({ data }) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
+  // Disable scrolling when news content is clicked
   useEffect(() => {
     if (isDialogOpen) {
-      // Disable scrolling
       document.body.style.overflow = "hidden";
     } else {
-      // Re-enable scrolling
       document.body.style.overflow = "auto";
     }
 
@@ -44,62 +44,90 @@ const News: React.FC<newsDummyData> = ({ data }) => {
 
   return (
     <>
-      <div className="flex flex-col gap-3 pl-8 pr-8">
-        {data.map((news) => (
-          <div
-            className="flex items-center gap-2 border-2 border-b-200 bg-gray-100 rounded-lg pl-4 pr-4 py-2"
-            onClick={openDialog}
-          >
-            <div className="buttons self-start">
-              <div className="flex gap-1 text-xl font-bold ">
-                <PiDotsSixVerticalBold className="w-4 text-b-900 h-8" />
-                <input
-                  className="w-3.5"
-                  type="checkbox"
-                  name=""
-                  id=""
-                  onClick={(e) => {
-                    e.stopPropagation();
-                  }}
-                />
-              </div>
-            </div>
-            <div className="news-information">
-              <h1 className="font-bold">{news.title}</h1>
-              <div className="author-date flex gap-3">
-                <span className="flex justify-center items-center gap-1">
-                  <PiUserCircleFill className="text-green-500" />
-                  <p>{news.author}</p>
-                </span>
-                <span className="flex justify-center items-center gap-1">
-                  <MdOutlineDateRange className="text-green-500" />
-                  <p>{news.date}</p>
-                </span>
-              </div>
-              <div className="content-summary">
-                <p className="flex gap-1">
-                  {news.content.split(" ").slice(0, 11).join(" ")}{" "}
-                  {news.content.split(" ")[12]}...{" "}
-                  <span className="flex items-center text-blue-600">
-                    {" "}
-                    <AiFillEye /> <p> See More</p>
-                  </span>
-                </p>
-              </div>
-            </div>
-            <div className="news-tags flex gap-2 text-green-500 ml-auto self-start">
-              <span className="news-tag border-2 border-green-200 bg-green-100 rounded-lg p-1">
-                <p>#Sports</p>
-              </span>
-              <span className="news-tag border-2 border-green-200 bg-green-100 rounded-lg p-1">
-                <p>#Worldwide</p>
-              </span>
-              <span className="news-tag border-2 border-green-200 bg-green-100 rounded-lg p-1">
-                <p>#Local</p>
-              </span>
-            </div>
+      <div id="navbar" className="flex justify-between items-center p-3">
+        <div className="navigation-functions flex flex-col gap-1">
+          <h1 className="font-bold text-2xl">News Articles</h1>
+          <div className="flex gap-3">
+            <input className="w-3.5" type="checkbox" name="" id="" />
+            <button className="p-1 rounded-lg border-2 border-blue-400 bg-blue-200 text-blue-500 w-20">
+              Publish
+            </button>
+            <button className="p-1 rounded-lg border-2 border-red-400 bg-red-200 text-red-400 w-20">
+              Delete
+            </button>
           </div>
-        ))}
+        </div>
+        <div className="navigation-search flex items-end">
+          <input
+            className="rounded-md border-2 border-#F3F5F7-500 p-1 w-80"
+            type="search"
+            name="email-search"
+            id=""
+            placeholder="Search..."
+          />
+        </div>
+      </div>
+      <div id="newsList" className="flex flex-col gap-3 pl-8 pr-8">
+        {data &&
+          data.map((news, index) => (
+            <div
+              className="flex items-center gap-2 border-2 border-b-200 bg-gray-100 rounded-lg pl-4 pr-4 py-2"
+              onClick={() => {
+                openDialog();
+                setSelectedIndex(index);
+              }}
+              key={news.id}
+            >
+              <div className="buttons self-start">
+                <div className="flex gap-1 text-xl font-bold ">
+                  <PiDotsSixVerticalBold className="w-4 text-b-900 h-8" />
+                  <input
+                    className="w-3.5"
+                    type="checkbox"
+                    name=""
+                    id=""
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="news-information">
+                <h1 className="font-bold">{news.title}</h1>
+                <div className="author-date flex gap-3">
+                  <span className="flex justify-center items-center gap-1">
+                    <PiUserCircleFill className="text-green-500" />
+                    <p>{news.author}</p>
+                  </span>
+                  <span className="flex justify-center items-center gap-1">
+                    <MdOutlineDateRange className="text-green-500" />
+                    <p>{news.date}</p>
+                  </span>
+                </div>
+                <div className="content-summary">
+                  <p className="flex gap-1">
+                    {news.content.split(" ").slice(0, 11).join(" ")}{" "}
+                    {news.content.split(" ")[12]}...{" "}
+                    <span className="flex items-center text-blue-600">
+                      {" "}
+                      <AiFillEye /> <p> See More</p>
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <div className="news-tags flex gap-2 text-green-500 ml-auto self-start">
+                <span className="news-tag border-2 border-green-200 bg-green-100 rounded-lg p-1">
+                  <p>#Sports</p>
+                </span>
+                <span className="news-tag border-2 border-green-200 bg-green-100 rounded-lg p-1">
+                  <p>#Worldwide</p>
+                </span>
+                <span className="news-tag border-2 border-green-200 bg-green-100 rounded-lg p-1">
+                  <p>#Local</p>
+                </span>
+              </div>
+            </div>
+          ))}
         <div
           className={`fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 ${
             isDialogOpen ? "block" : "hidden"
@@ -109,19 +137,19 @@ const News: React.FC<newsDummyData> = ({ data }) => {
             id="ShowMore"
             open={isDialogOpen}
             onClose={closeDialog}
-            className="w-[500px] h-auto rounded-lg p-10 fixed top-[25%]"
+            className="w-[500px] h-[500px] overflow-auto rounded-lg p-10 fixed top-[25%]"
           >
             <h1 className=" flex justify-between font-bold text-[1.2rem]">
-              {data[1].title}{" "}
+              {data[selectedIndex].title}
               <span onClick={closeDialog}>
                 <AiOutlineCloseCircle />
               </span>
             </h1>
             <p className="my-3 font-bold text-gray-400">
-              {data[1].author} | {data[1].date}
+              {data[selectedIndex].author} | {data[selectedIndex].date}
             </p>
             <p className="border-dotted border-2 border-black p-3">
-              {data[1].content}
+              {data[selectedIndex].content}
             </p>
             <div className="flex gap-1 justify-center my-2">
               <button className="p-1 rounded-lg border-2 border-blue-400 bg-blue-400 text-white w-15">
